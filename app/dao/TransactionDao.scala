fa8d71f296599fe56b663db6b97cf002f3b95f71
Denis Chaplygin <dchaplyg@redhat.com>
2017-04-02 15:17:47 +0200

    No more asyncs in transaction filter.

--- app/dao/TransactionDao.scala
+++ app/dao/TransactionDao.scala
@@ -46,37 +46,53 @@ class TransactionDao @Inject()(protected val dbConfigProvider: DatabaseConfigPro
   }
 
   def list(filter: TransactionFilter, sort: Seq[SortBy], page: Option[Page]): Future[Seq[Transaction]] = {
-    val tag_tx = filter.tag.map { f=>
-      Await.result(db.run(tags.filter(_.txtag inSet f).map(_.id).result.flatMap(tag_ids => {
+    val tag_tx_action = filter.tag.map { f=>
+      tags.filter(_.txtag inSet f).map(_.id).result.flatMap(tag_ids => {
         tagMaps.filter(_.tag_id inSet tag_ids).map(_.tx_id).result
-      })), 500 millis)
-    }
+      })
+    }.getOrElse(DBIO.successful(Seq.empty[Long]))
 
-    val acc_tx = filter.account_id.map { a =>
-      Await.result(db.run(operations.filter(_.account_id inSet  a).map(_.tx_id).result), 500 milli)
-    }
+    val acc_tx_action = filter.account_id.map { a=>
+      operations.filter(_.account_id inSet  a).map(_.tx_id).result
+    }.getOrElse(DBIO.successful(Seq.empty[Long]))
 
-    val query = transactions.filter { t  =>
-      List(
-        filter.comment.map(t.comment.getOrElse("") === _),
-        filter.notEarlier.map(t.timestamp >= _),
-        filter.notLater.map(t.timestamp <= _),
-        tag_tx.map(t.id inSet _),
-        acc_tx.map(t.id inSet _)
-      ).collect({ case Some(x) => x }).reduceLeftOption(_ && _).getOrElse(true: Rep[Boolean])
-    }
+    val preActions = tag_tx_action zip acc_tx_action
 
-    val sortedQuery = sort.headOption.getOrElse(SortBy("timestamp", Desc)) match {
-      case SortBy("timestamp", Asc) => query.sortBy(_.timestamp.asc)
-      case _ => query.sortBy(_.timestamp.desc)
-    }
 
-    val pagedQuery = page match {
-      case Some(p) => sortedQuery.drop(p.size*(p.no-1)).take(p.size)
-      case None => sortedQuery
-    }
+    val query = preActions.flatMap(tx_id_s => {
+      val (tag_tx_s, acc_tx_s) = tx_id_s
 
-    db.run(pagedQuery.result)
+      //We need tag_tx_s and acc_tx_s lists
+      //to be applied conditionally, depending
+      //on presence of list contents
+
+      val tag_tx = Option(tag_tx_s).filter(_.nonEmpty)
+      val acc_tx = Option(acc_tx_s).filter(_.nonEmpty)
+
+      val criteriaQuery = transactions.filter { t  =>
+        List(
+          filter.comment.map(t.comment.getOrElse("") === _),
+          filter.notEarlier.map(t.timestamp >= _),
+          filter.notLater.map(t.timestamp <= _),
+          tag_tx.map(t.id inSet _),
+          acc_tx.map(t.id inSet _)
+        ).collect({ case Some(x) => x }).reduceLeftOption(_ && _).getOrElse(true: Rep[Boolean])
+      }
+
+      val sortedQuery = sort.headOption.getOrElse(SortBy("timestamp", Desc)) match {
+        case SortBy("timestamp", Asc) => criteriaQuery.sortBy(_.timestamp.asc)
+        case _ => criteriaQuery.sortBy(_.timestamp.desc)
+      }
+
+      val pagedQuery = page match {
+        case Some(p) => sortedQuery.drop(p.size*(p.no-1)).take(p.size)
+        case None => sortedQuery
+      }
+
+      pagedQuery.result
+    })
+
+    db.run(query)
   }
 
   def findById(id: Long): Future[Option[Transaction]] = {
