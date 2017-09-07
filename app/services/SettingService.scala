c39c3a6e16efcb96917f0c461b7e1566a48dbb2b
Denis Chaplygin <dchaplyg@redhat.com>
2017-09-07 17:09:55 +0200

    Pattern matching in SettingService could be replaces with map/getOrElse calls

--- app/services/SettingService.scala
+++ app/services/SettingService.scala
@@ -40,34 +40,14 @@ object SettingService {
       .map(_.right)
       .getOrElse("SETTING_DATA_INVALID".left)
       .map(CurrencyDao.findById)
-    val haveCurrency = curOption.transform.flatMap {
-      case Some(c) => c.right
-      case None => "SETTING_DATA_INVALID".left
-    }
+    val haveCurrency = curOption.transform.flatMap(_.map(_.right).getOrElse("SETTING_DATA_INVALID".left))
 
-    val setting = haveCurrency.map { _ =>
-      SettingDao.findById("currency.primary")
-    }
-    val haveSettings = setting.map { o =>
-      val i = o.map {
-        case Some(s) => s.right
-        case None => "SETTING_NOT_FOUND".left
-      }
-      i
-    }
-    val newSetting = haveSettings.map(o => EitherD(o)).flatten.map { s =>
-      s.copy(value = value.getOrElse(s.value))
-    }
-    val savedSetting = newSetting.map { s =>
-      SettingDao.update(s)
-    }
+    val setting = haveCurrency.map(_ => SettingDao.findById("currency.primary").map(_.map(_.right).getOrElse("SETTING_NOT_FOUND".left)))
 
-    val result = savedSetting.map { o =>
-      EitherD(o.map {
-        case Some(s) => s.right
-        case None => "ACCOUNT_NOT_UPDATED".left
-      })
-    }
-    result.flatten
+    val savedSetting = setting.map(o => EitherD(o)).flatten
+      .map(s => s.copy(value = value.getOrElse(s.value)))
+      .map(s => SettingDao.update(s))
+
+    savedSetting.map(_.map(_.map(_.right).getOrElse("ACCOUNT_NOT_UPDATED".left))).map(o => EitherD(o)).flatten
   }
 }
