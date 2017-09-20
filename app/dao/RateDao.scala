7eb6ac024dad83c3c9d70aee82e782b8cde303e1
Denis Chaplygin <dchaplyg@redhat.com>
2017-09-20 17:37:58 +0200

    Added Dao object for Rate.

---
+++ app/dao/RateDao.scala
@@ -0,0 +1,24 @@
+package dao
+
+import java.time.LocalDateTime
+
+import dao.tables.Rates
+import models.Rate
+import dao.mappers.LocalDateMapper._
+import slick.driver.PostgresDriver.api._
+
+object RateDao {
+  val rates = TableQuery[Rates]
+
+  def list(ts: LocalDateTime): DBIO[Seq[Rate]] =
+    rates.filter(_.beginning <= ts).filter(_.end > ts).result
+
+  def findByPair(ts: LocalDateTime, from: Long, to: Long): DBIO[Option[Rate]] =
+    rates
+      .filter(_.beginning <= ts)
+      .filter(_.end > ts)
+      .filter(_.from_currency === from)
+      .filter(_.to_currency === to)
+      .result
+      .headOption
+}
