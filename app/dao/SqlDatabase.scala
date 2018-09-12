83deff80e0c1ad51a31b3ea83ff410c12f731b00
Denis Chaplygin <dchaplyg@redhat.com>
2018-09-12 16:23:47 +0200

    Made a separa database querying class.

---
+++ app/dao/SqlDatabase.scala
@@ -0,0 +1,18 @@
+package dao
+
+import akka.actor.ActorSystem
+import javax.inject.Inject
+import play.api.db.slick._
+import play.api.libs.concurrent.CustomExecutionContext
+import slick.dbio.DBIO
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent._
+
+class SqlExecutionContext @Inject()(actorSystem: ActorSystem)
+  extends CustomExecutionContext(actorSystem, "mdg.sql.dispatcher")
+
+class SqlDatabase @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: SqlExecutionContext)
+  extends HasDatabaseConfigProvider[JdbcProfile] {
+  def query[T](q: DBIO[T]): Future[T] = db.run(q)
+}
