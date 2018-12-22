9c07ee11b03b73d70971351f2d3165e4f41d4f22
Denis Chaplygin <dchaplyg@redhat.com>
2018-12-22 14:36:43 +0100

    Extracted date period expander

--- app/reports/AssetByCurrencyReport.scala
+++ app/reports/AssetByCurrencyReport.scala
@@ -11,20 +11,34 @@ import scala.concurrent.Future
 
 class AssetByCurrencyReport @Inject() (protected val sql: SqlDatabase)
                                       (implicit ec: SqlExecutionContext){
-  def calculate(start: LocalDate, end: LocalDate, granularity: Int): Future[Seq[(LocalDate, Seq[(Long,BigDecimal)])] = {
-    val numberOfDays = ChronoUnit.DAYS.between(start, end)/granularity
-    val daysGenerated = for (f <- 0L to numberOfDays) yield start.plusDays(f*granularity)
+
+  /**
+    * Takes period of two dates and granularity
+    * and generated list of days between those two days
+    * with specified granularity.
+    * @param start First date of the period (inclusive)
+    * @param end Last date of the period (inclusive)
+    * @param granularity quantity of days between two dates
+    * @return A sorted array of dates between start and end, filled
+    *         with days, that have granularity days between them.
+    */
+  private def expandPeriod(start: LocalDate,
+                           end: LocalDate,
+                           granularity: Int) = {
+    val numberOfDays = ChronoUnit.DAYS.between(start, end) / granularity
+    val daysGenerated = for (f <- 0L to numberOfDays)
+      yield start.plusDays(f * granularity)
     val days = daysGenerated.:+(end)
-
-    val series = days.map(d => sql.query(AssetQuery.getTotalAssetsByCurrencyForDate(d)).map((d,_)))
-
-    Future.sequence(series)
+    days
   }
 
-  def get(start: LocalDate, end: LocalDate, granularity: Int): Future[GenericReportDTO[AssetByCurrencyReportEntry]] = {
-    val report = this.calculate(start, end, granularity)
-    val detailed = report.map(_.map(e => (e._1, e._2.map(d => AssetByCurrencyReportDetail(d._1, d._2)))))
-    val entries = detailed.map(s => s.map(e => AssetByCurrencyReportEntry(e._1, e._2)))
+
+def get(start: LocalDate, end: LocalDate, granularity: Int): Future[GenericReportDTO[AssetByCurrencyReportEntry]] = {
+  val series = expandPeriod(start, end, granularity)
+    .map(d => sql.query(AssetQuery.getTotalAssetsByCurrencyForDate(d)).map((d,_)))
+  val report = Future.sequence(series)
+  val detailed = report.map(_.map(e => (e._1, e._2.map(d => AssetByCurrencyReportDetail(d._1, d._2)))))
+  val entries = detailed.map(s => s.map(e => AssetByCurrencyReportEntry(e._1, e._2)))
     entries.map(GenericReportDTO(Some("asset_by_currency"), _))
   }
 }
