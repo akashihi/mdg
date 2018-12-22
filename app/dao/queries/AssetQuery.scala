a7aac8233b39438ee8623662bb50831e2ca6e915
Denis Chaplygin <dchaplyg@redhat.com>
2018-12-22 21:14:08 +0100

    Status reporting should include hidden accounts

--- app/dao/queries/AssetQuery.scala
+++ app/dao/queries/AssetQuery.scala
@@ -14,7 +14,7 @@ object AssetQuery {
     * @param term date on which remain is calculated
     * @return remains on that date
     */
-  def getTotalAssetsForDate(term: LocalDate)(implicit ec: ExecutionContext): DBIO[BigDecimal] = {
+  def getTotalAssetsForDate(term: LocalDate, includeHidden: Boolean = false)(implicit ec: ExecutionContext): DBIO[BigDecimal] = {
     val dt = Date.valueOf(term)
     sql"""
          select sum(o.amount*coalesce(r.rate,1))
@@ -23,7 +23,7 @@ object AssetQuery {
          inner join tx on (o.tx_id=tx.id)
          inner join setting as s on (s.name='currency.primary')
          left outer join rates as r on (r.from_id=a.currency_id and r.to_id=s.value::bigint and r.rate_beginning <= now() and r.rate_end > now())
-         where a.account_type='asset' and a.hidden='f' and tx.ts <  $dt"""
+         where a.account_type='asset' and a.hidden=$includeHidden and tx.ts <  $dt"""
       .as[Option[BigDecimal]]
       .map(_.head)
       .map(_.getOrElse(BigDecimal(0)))
@@ -36,7 +36,7 @@ object AssetQuery {
          from operation as o
          left outer join account as a on(o.account_id = a.id)
          inner join tx on (o.tx_id=tx.id)
-         where a.account_type='asset' and a.hidden='f' and tx.ts < $dt
+         where a.account_type='asset' and tx.ts < $dt
          group by a.currency_id"""
       .as[(Long, BigDecimal)]
   }
@@ -51,7 +51,7 @@ object AssetQuery {
          inner join tx on (o.tx_id=tx.id)
          inner join setting as s on (s.name='currency.primary')
          left outer join rates as r on (r.from_id=a.currency_id and r.to_id=s.value::bigint and r.rate_beginning <= now() and r.rate_end > now())
-         where a.account_type='asset' and a.hidden='f' and tx.ts < $dt group by ap.asset_type"""
+         where a.account_type='asset' and tx.ts < $dt group by ap.asset_type"""
       .as[(String, BigDecimal)]
   }
 }
