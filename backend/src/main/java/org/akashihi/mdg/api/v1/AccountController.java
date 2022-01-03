82f6d5e175c92ffd7ebef69f68e539e209f6e245
Denis Chaplygin <akashihi@gmail.com>
2022-01-03 18:01:30 +0200

    Solved code duplication in AccountController V1

--- backend/src/main/java/org/akashihi/mdg/api/v1/AccountController.java
+++ backend/src/main/java/org/akashihi/mdg/api/v1/AccountController.java
@@ -10,12 +10,47 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+import java.util.function.Function;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
     private final ObjectMapper objectMapper;
+
+    protected Optional<Map<String, String>> buildFilter(Optional<String> query) {
+        return query.map(s -> {
+            try {
+                var queryMap = new HashMap<String,String>();
+                var parsedQuery = objectMapper.readValue(s, Map.class);
+                parsedQuery
+                        .keySet().stream().filter(k -> k instanceof String && parsedQuery.get(k) instanceof String)
+                        .forEach(k -> queryMap.put((String) k, (String) parsedQuery.get(k)));
+                return queryMap;
+            } catch (JsonProcessingException e) {
+                return Collections.EMPTY_MAP;
+            }
+        });
+    }
+
+    protected Function<Account, Account> embedAccountObjects(Optional<Collection<String>> embed) {
+        var categories = embed.map(e -> e.contains("category")).orElse(false);
+        var currencies = embed.map(e -> e.contains("currency")).orElse(false);
+
+        return (account) -> {
+            account.setCurrencyId(account.getCurrency().getId());
+            if (!currencies) {
+                account.setCurrency(null);
+            }
+            if (account.getCategory() != null) {
+                account.setCategoryId(account.getCategory().getId());
+                if (!categories) {
+                    account.setCategory(null);
+                }
+            }
+            return account;
+        };
+    }
 
     @PostMapping(value = "/accounts", consumes = "application/vnd.mdg+json;version=1", produces = "application/vnd.mdg+json;version=1")
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,31 +65,8 @@ public class AccountController {
     Accounts list(@RequestParam("q") Optional<String> query, @RequestParam("embed") Optional<Collection<String>> embed) {
         var embed_categories = embed.map(e -> e.contains("category")).orElse(false);
         var embed_currencies = embed.map(e -> e.contains("currency")).orElse(false);
-        Optional<Map<String, String>> filter = query.map(s -> {
-            try {
-                var queryMap = new HashMap<String,String>();
-                var parsedQuery = objectMapper.readValue(s, Map.class);
-                parsedQuery
-                        .keySet().stream().filter(k -> k instanceof String && parsedQuery.get(k) instanceof String)
-                        .forEach(k -> queryMap.put((String) k, (String) parsedQuery.get(k)));
-                return queryMap;
-            } catch (JsonProcessingException e) {
-                return Collections.EMPTY_MAP;
-            }
-        });
 
-        var accounts = accountService.list(filter).stream().peek(a -> {
-            a.setCurrencyId(a.getCurrency().getId());
-            if (!embed_currencies) {
-                a.setCurrency(null);
-            }
-            if (a.getCategory() != null) {
-                a.setCategoryId(a.getCategory().getId());
-                if (!embed_categories) {
-                    a.setCategory(null);
-                }
-            }
-        }).toList();
+        var accounts = accountService.list(buildFilter(query)).stream().map(embedAccountObjects(embed)).toList();
         return new Accounts(accounts);
     }
 
@@ -62,19 +74,7 @@ public class AccountController {
     Account get(@PathVariable("id") Long id, @RequestParam("embed") Optional<Collection<String>> embed) {
         var embed_categories = embed.map(e -> e.contains("category")).orElse(false);
         var embed_currencies = embed.map(e -> e.contains("currency")).orElse(false);
-        return accountService.get(id).map(a -> {
-            a.setCurrencyId(a.getCurrency().getId());
-            if (!embed_currencies) {
-                a.setCurrency(null);
-            }
-            if (a.getCategory() != null) {
-                a.setCategoryId(a.getCategory().getId());
-                if (!embed_categories) {
-                    a.setCategory(null);
-                }
-            }
-            return a;
-        }).orElseThrow(() -> new RestException("ACCOUNT_NOT_FOUND", 404, "/accounts/%d".formatted(id)));
+        return accountService.get(id).map(embedAccountObjects(embed)).orElseThrow(() -> new RestException("ACCOUNT_NOT_FOUND", 404, "/accounts/%d".formatted(id)));
     }
 
     @PutMapping(value = "/accounts/{id}", consumes = "application/vnd.mdg+json;version=1", produces = "application/vnd.mdg+json;version=1")
