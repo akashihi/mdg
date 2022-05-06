1916f67d70db29e9522a04021828271ed56a2d9c
Denis Chaplygin <akashihi@gmail.com>
2022-05-06 23:35:31 +0300

    Budget Entry API v0 added

--- backend/src/main/java/org/akashihi/mdg/api/v0/BudgetControllerV0.java
+++ backend/src/main/java/org/akashihi/mdg/api/v0/BudgetControllerV0.java
@@ -1,12 +1,10 @@
 package org.akashihi.mdg.api.v0;
 
 import lombok.RequiredArgsConstructor;
-import org.akashihi.mdg.api.v0.dto.BudgetData;
-import org.akashihi.mdg.api.v0.dto.DataPlural;
-import org.akashihi.mdg.api.v0.dto.DataSingular;
-import org.akashihi.mdg.api.v0.dto.RequestException;
+import org.akashihi.mdg.api.v0.dto.*;
 import org.akashihi.mdg.api.v1.RestException;
 import org.akashihi.mdg.entity.Budget;
+import org.akashihi.mdg.entity.BudgetEntry;
 import org.akashihi.mdg.service.BudgetService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +23,18 @@ public class BudgetControllerV0 {
 
     private BudgetData.Attributes toDto(Budget budget) {
         return new BudgetData.Attributes(budget.getBeginning().format(DateTimeFormatter.ISO_LOCAL_DATE), budget.getEnd().format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
+    private BudgetEntry fromDto(BudgetEntryData data) {
+        var entry = new BudgetEntry();
+        entry.setExpectedAmount(data.getAttributes().expected_amount());
+        entry.setEvenDistribution(data.getAttributes().even_distribution());
+        entry.setProration(data.getAttributes().proration());
+        return entry;
+    }
+
+    private BudgetEntryData.Attributes toDto(BudgetEntry entry) {
+        return new BudgetEntryData.Attributes(entry.getAccount().getId(), entry.getAccount().getName(), entry.getAccount().getAccountType().toDbValue(), entry.getEvenDistribution(), entry.getProration(), entry.getExpectedAmount(), entry.getActualAmount(), entry.getAllowedSpendings());
     }
 
     @GetMapping(value = "/api/budget", produces = "application/vnd.mdg+json")
@@ -66,4 +76,26 @@ public class BudgetControllerV0 {
         budgetService.delete(id);
     }
 
+    @GetMapping(value = "/api/budget/{id}/entry/{entryId}", produces = "application/vnd.mdg+json")
+    DataSingular<BudgetEntryData> getEntry(@PathVariable("id") Long id, @PathVariable("entryId") Long entryId) {
+        var entry =  budgetService.getBudgetEntry(entryId).orElseThrow(() -> new RestException("BUDGETENTRY_NOT_FOUND", 404, "/api/budget/%d/entry/%d".formatted(id, entryId)));
+        return new DataSingular<>(new BudgetEntryData(entry.getId(), "budgetentry", toDto(entry)));
+    }
+
+
+    @GetMapping(value = "/api/budget/{id}/entry", produces = "application/vnd.mdg+json")
+    DataPlural<BudgetEntryData> listEntry(@PathVariable("id") Long id) {
+        return new DataPlural<>(budgetService.listEntries(id).stream().map(b -> new BudgetEntryData(b.getId(), "budgetentry", toDto(b))).toList());
+    }
+
+    @PutMapping(value = "/api/budget/{id}/entry/{entryId}", consumes = {"application/vnd.mdg+json", "application/json"}, produces = "application/vnd.mdg+json")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    DataSingular<BudgetEntryData> updateEntry(@PathVariable("entryId") Long entryId, @RequestBody DataSingular<BudgetEntryData> data) {
+        try {
+            var entry = budgetService.updateBudgetEntry(entryId, fromDto(data.data())).orElseThrow(() -> new RequestException(404, "BUDGETENTRY_NOT_FOUND"));
+            return new DataSingular<>(new BudgetEntryData(entry.getId(), "budgetentry", toDto(entry)));
+        } catch (RestException ex) {
+            throw new RequestException(ex.getStatus(), ex.getTitle());
+        }
+    }
 }
