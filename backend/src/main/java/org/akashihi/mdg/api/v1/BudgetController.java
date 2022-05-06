d15a4ef8b7b587fa166a9fa6555604bb23b16439
Denis Chaplygin <akashihi@gmail.com>
2022-05-06 22:26:57 +0300

    Simplified BudgetEntryTree analyzis

--- backend/src/main/java/org/akashihi/mdg/api/v1/BudgetController.java
+++ backend/src/main/java/org/akashihi/mdg/api/v1/BudgetController.java
@@ -6,18 +6,20 @@ import org.akashihi.mdg.api.v1.dto.BudgetEntryTree;
 import org.akashihi.mdg.api.v1.dto.BudgetEntryTreeEntry;
 import org.akashihi.mdg.api.v1.dto.Budgets;
 import org.akashihi.mdg.api.v1.filtering.Embedding;
-import org.akashihi.mdg.entity.*;
+import org.akashihi.mdg.entity.AccountType;
+import org.akashihi.mdg.entity.Budget;
+import org.akashihi.mdg.entity.BudgetEntry;
+import org.akashihi.mdg.entity.Category;
 import org.akashihi.mdg.service.BudgetService;
 import org.akashihi.mdg.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,42 +27,36 @@ public class BudgetController {
     private final BudgetService budgetService;
     private final CategoryService categoryService;
 
-    private record Subtotal(BudgetEntryTreeEntry entry, BudgetEntryMode mode, BigDecimal actual, BigDecimal expected) {}
+    protected static <T> BigDecimal getTotals(Function<T,BigDecimal> f, Collection<T> entries) {
+        return entries.stream().map(f).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 
-    public static BudgetEntryTreeEntry convertTopCategory(AccountType accountType, Collection<Category> categories, Collection<BudgetEntry> entries, Optional<Collection<String>> embed, LocalDate from, LocalDate to, LocalDate forDay) {
+    protected static BudgetEntryTreeEntry convertTopCategory(AccountType accountType, Collection<Category> categories, Collection<BudgetEntry> entries, Optional<Collection<String>> embed) {
         var topEntries = entries.stream().filter(e -> e.getAccount().getAccountType().equals(accountType))
                 .filter(e -> Objects.isNull(e.getAccount().getCategory()))
                 .map(Embedding.embedBudgetEntryObject(embed))
                 .toList();
-        var topCategories = categories.stream().filter(a -> a.getAccountType().equals(accountType)).map(c -> convertCategory(c, entries, embed, from, to, forDay)).filter(Optional::isPresent).map(Optional::get).toList();
-        var mode = BudgetEntryMode.flatten(topCategories.stream().map(Subtotal::mode).toList());
-        var actualSpendingsCategories = topCategories.stream().map(Subtotal::actual).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var expectedSpendingsCategories = topCategories.stream().map(Subtotal::expected).reduce(BigDecimal.ZERO, BigDecimal::add);
+        var topCategories = categories.stream().filter(a -> a.getAccountType().equals(accountType)).map(c -> convertCategory(c, entries, embed)).filter(Optional::isPresent).map(Optional::get).toList();
+        var actualSpendingsCategories = getTotals(BudgetEntryTreeEntry::actualAmount, topCategories);
+        var expectedSpendingsCategories = getTotals(BudgetEntryTreeEntry::expectedAmount, topCategories);
+        var allowedSpendingsCategories = getTotals(BudgetEntryTreeEntry::allowedSpendings, topCategories);
         var percent = BudgetService.getSpendingPercent(actualSpendingsCategories, expectedSpendingsCategories);
-        var allowedSpendings = BudgetService.getAllowedSpendings(actualSpendingsCategories, expectedSpendingsCategories, from, to, forDay, mode);
-        return new BudgetEntryTreeEntry(null, null, actualSpendingsCategories, expectedSpendingsCategories, percent, allowedSpendings, topEntries, topCategories.stream().map(Subtotal::entry).toList());
+        return new BudgetEntryTreeEntry(null, null, actualSpendingsCategories, expectedSpendingsCategories, percent, allowedSpendingsCategories, topEntries, topCategories);
     }
 
-    public static Optional<Subtotal> convertCategory(Category category, Collection<BudgetEntry> entries, Optional<Collection<String>> embed, LocalDate from, LocalDate to, LocalDate forDay) {
+    protected static Optional<BudgetEntryTreeEntry> convertCategory(Category category, Collection<BudgetEntry> entries, Optional<Collection<String>> embed) {
         var categoryEntries = entries.stream().filter(e -> e.getAccount().getCategory() != null).filter(e -> e.getAccount().getCategory().equals(category)).map(Embedding.embedBudgetEntryObject(embed)).toList();
-        var subCategories = category.getChildren().stream().map(c -> convertCategory(c, entries, embed, from, to, forDay)).filter(Optional::isPresent).map(Optional::get).toList();
-        var mode = BudgetEntryMode.flatten(Stream.concat(subCategories.stream().map(Subtotal::mode), categoryEntries.stream().map(BudgetEntryMode::from)).toList());
-        var actualSpendingsEntries = categoryEntries.stream().map(BudgetEntry::getActualAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var actualSpendingsCategories = subCategories.stream().map(Subtotal::actual).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var actualSpendings = actualSpendingsEntries.add(actualSpendingsCategories);
-
-        var expectedSpendingsEntries = categoryEntries.stream().map(BudgetEntry::getExpectedAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var expectedSpendingsCategories = subCategories.stream().map(Subtotal::expected).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var expectedSpendings = expectedSpendingsEntries.add(expectedSpendingsCategories);
+        var subCategories = category.getChildren().stream().map(c -> convertCategory(c, entries, embed)).filter(Optional::isPresent).map(Optional::get).toList();
 
         if (categoryEntries.isEmpty() && subCategories.isEmpty()) {
             return Optional.empty();
-        } else {
-            var percent = BudgetService.getSpendingPercent(actualSpendings, expectedSpendings);
-            var allowedSpendings = BudgetService.getAllowedSpendings(actualSpendings, expectedSpendings, from, to, forDay, mode);
-            var upperEntry = new BudgetEntryTreeEntry(category.getId(), category.getName(), actualSpendings, expectedSpendings, percent, allowedSpendings, categoryEntries, subCategories.stream().map(Subtotal::entry).toList());
-            return Optional.of(new Subtotal(upperEntry, mode, actualSpendings, expectedSpendings));
         }
+        var actualSpendings = getTotals(BudgetEntry::getActualAmount, categoryEntries).add(getTotals(BudgetEntryTreeEntry::actualAmount, subCategories));
+        var expectedSpendings = getTotals(BudgetEntry::getExpectedAmount, categoryEntries).add(getTotals(BudgetEntryTreeEntry::expectedAmount, subCategories));
+        var allowedSpendings = getTotals(BudgetEntry::getAllowedSpendings, categoryEntries).add(getTotals(BudgetEntryTreeEntry::allowedSpendings, subCategories));
+
+        var percent = BudgetService.getSpendingPercent(actualSpendings, expectedSpendings);
+        return Optional.of(new BudgetEntryTreeEntry(category.getId(), category.getName(), actualSpendings, expectedSpendings, percent, allowedSpendings, categoryEntries, subCategories));
     }
 
     @PostMapping(value = "/budgets", consumes = "application/vnd.mdg+json;version=1", produces = "application/vnd.mdg+json;version=1")
@@ -106,8 +102,8 @@ public class BudgetController {
             entries = entries.stream().filter(e -> !(e.getActualAmount().compareTo(BigDecimal.ZERO)==0 && e.getExpectedAmount().compareTo(BigDecimal.ZERO)==0)).toList();
         }
 
-        var expenseEntry = convertTopCategory(AccountType.EXPENSE, categories, entries, embed, budget.getBeginning(), budget.getEnd(), LocalDate.now());
-        var incomeEntry = convertTopCategory(AccountType.INCOME, categories, entries, embed, budget.getBeginning(), budget.getEnd(), LocalDate.now());
+        var expenseEntry = convertTopCategory(AccountType.EXPENSE, categories, entries, embed);
+        var incomeEntry = convertTopCategory(AccountType.INCOME, categories, entries, embed);
         return new BudgetEntryTree(expenseEntry ,incomeEntry);
     }
 
