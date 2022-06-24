99fbefef825772c93537439e38ca65bb6e0c3664
Denis Chaplygin <akashihi@gmail.com>
2022-06-24 22:47:04 +0300

    Fixed PMD warning

--- backend/src/main/java/org/akashihi/mdg/service/CategoryService.java
+++ backend/src/main/java/org/akashihi/mdg/service/CategoryService.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Optional;
 
 @Service
