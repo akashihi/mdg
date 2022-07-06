15616d55dc371b32369990c63678df10a8abeeae
Denis Chaplygin <akashihi@gmail.com>
2022-07-06 17:21:48 +0300

    Removed obsolete reporting support code

--- frontend/src/js/actions/ReportActions.ts
+++ frontend/src/js/actions/ReportActions.ts
@@ -2,12 +2,7 @@ import { Action } from 'redux';
 
 import { checkApiError, parseJSON } from '../util/ApiUtils'
 
-import {
-    GET_BUDGETREPORT_REQUEST,
-    GET_BUDGETREPORT_SUCCESS,
-    GET_BUDGETREPORT_FAILURE,
-    ReportActionType
-} from '../constants/Report'
+import { ReportActionType } from '../constants/Report'
 import {TotalsReport} from "../models/Report";
 
 export interface ReportAction extends Action {
@@ -28,37 +23,4 @@ export function loadTotalsReport () {
         dispatch({type: ReportActionType.TotalsReportStore, payload: {totals: map.report}})
       })
   }
-}
-
-export function loadBudgetExecutionReport () {
-  /*return (dispatch, getState) => {
-    dispatch({
-      type: GET_BUDGETREPORT_REQUEST,
-      payload: true
-    })
-
-    const url = '/api/report/budget/execution' + reportDatesToParams(getState)
-
-    fetch(url)
-      .then(parseJSON)
-      .then(checkApiError)
-      .then(function (json) {
-        const dates = Immutable.fromJS(json.data.attributes.value.map(item => item.date))
-        const aIncome = Immutable.fromJS(json.data.attributes.value.map(item => item.income.actual))
-        const eIncome = Immutable.fromJS(json.data.attributes.value.map(item => item.income.expected))
-        const aExpense = Immutable.fromJS(json.data.attributes.value.map(item => -1 * item.expense.actual))
-        const eExpense = Immutable.fromJS(json.data.attributes.value.map(item => -1 * item.expense.expected))
-        const profit = Immutable.fromJS(json.data.attributes.value.map(item => item.profit))
-        dispatch({
-          type: GET_BUDGETREPORT_SUCCESS,
-          payload: Map({ dates, aIncome, eIncome, aExpense, eExpense, profit })
-        })
-      })
-      .catch(function (response) {
-        dispatch({
-          type: GET_BUDGETREPORT_FAILURE,
-          payload: response.json
-        })
-      })
-  }*/
 }
--- frontend/src/js/constants/Report.ts
+++ frontend/src/js/constants/Report.ts
@@ -1,7 +1,3 @@
-export const GET_BUDGETREPORT_REQUEST = 'GET_BUDGETREPORT_REQUEST'
-export const GET_BUDGETREPORT_SUCCESS = 'GET_BUDGETREPORT_SUCCESS'
-export const GET_BUDGETREPORT_FAILURE = 'GET_BUDGETREPORT_FAILURE'
-
 export enum ReportActionType {
     TotalsReportLoad = 'TotalsReportLoad',
     TotalsReportStore = 'TotalsReportStore'
