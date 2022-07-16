9be2c0b199dcb2cfa03e3e68f3c55d56ccabc80c
Denis Chaplygin <akashihi@gmail.com>
2022-07-16 23:22:34 +0300

    Removed unused modules

--- frontend/src/js/util/ApiUtils.js
+++ frontend/src/js/util/ApiUtils.js
@@ -1,45 +1,8 @@
-import { OrderedMap, Map } from 'immutable'
-
 export function parseJSON (response) {
   try {
     return response.json()
   } catch (e) {
     // Not a json, pass
-  }
-}
-
-export function dataToMap (json) {
-  function mapChildren (content) {
-    if (content.has('children')) {
-      const children = content.get('children').reduce((subacc, item) => {
-        const mapItem = Map(item)
-        return subacc.set(item.id, mapChildren(mapItem))
-      }, new OrderedMap())
-      content = content.set('children', children)
-    }
-    return content
-  }
-
-  return json.data.reduce((acc, item) => {
-    let content = Map(item.attributes)
-
-    // Special handling for tree structures, like Category
-    content = mapChildren(content)
-    return acc.set(item.id, content)
-  }, new OrderedMap())
-}
-
-export function singleToMap (json) {
-  const attr = Map(json.data.attributes)
-  return OrderedMap().set(json.data.id, attr)
-}
-
-export function mapToData (id, item) {
-  return {
-    data: {
-      id,
-      attributes: item.toJS()
-    }
   }
 }
 
@@ -63,11 +26,4 @@ export function checkApiError (json) {
   } else {
     return json
   }
-}
-
-export function dateToYMD (date) {
-  const d = date.getDate()
-  const m = date.getMonth() + 1
-  const y = date.getFullYear()
-  return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d)
 }
