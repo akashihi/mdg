c650bc97f1ad8ba1b6a867c44ec576990a9c788c
Denis Chaplygin <akashihi@gmail.com>
2022-07-17 21:16:12 +0300

    Excluded 'allowed' on categories for income accounts

--- frontend/src/js/components/budget/BudgetEntry.tsx
+++ frontend/src/js/components/budget/BudgetEntry.tsx
@@ -40,7 +40,7 @@ export function BudgetCategoryEntry(props: { entry: BudgetEntryTreeNode; indent:
                     <div>{props.entry.name}</div>
                 </Grid>
                 <Grid item xs={3} sm={3} md={2} lg={1}>
-                    {props.entry.allowed_spendings > 0 ? `${props.entry.allowed_spendings} allowed` : ''}
+                    {accountType === 'EXPENSE' && props.entry.allowed_spendings > 0 ? `${props.entry.allowed_spendings} allowed` : ''}
                 </Grid>
                 <Grid item xs={2} sm={2} md={2} lg={1}>
                     <div style={{ width: '60px', height: '60px' }}>
