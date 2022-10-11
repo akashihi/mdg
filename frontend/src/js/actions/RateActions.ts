8cff4174085b85a735eae69263adfe812d85b5fe
Denis Chaplygin <akashihi@gmail.com>
2022-10-11 19:40:11 +0300

    Rates actions/reducers migrated to RTK creators

--- frontend/src/js/actions/RateActions.ts
+++ frontend/src/js/actions/RateActions.ts
@@ -1,23 +1,17 @@
-import { Action } from 'redux';
 import moment from 'moment';
 import * as API from '../api/api';
-import * as Model from '../api/model';
-import { RateActionsType } from '../constants/Rate';
 import { wrap } from './base';
-
-export interface RateAction extends Action {
-    payload: Model.Rate[];
-}
+import {RatesLoad, RatesStore} from '../reducers/RateReducer';
 
 export function loadRatesList() {
     return wrap(async dispatch => {
-        dispatch({ type: RateActionsType.RatesLoad, payload: {} });
+        dispatch(RatesLoad());
         const now = moment();
         const result = await API.listRates(now);
         if (result.ok) {
-            dispatch({ type: RateActionsType.RatesStore, payload: result.val });
+            dispatch(RatesStore(result.val));
         } else {
-            dispatch({ type: RateActionsType.RatesStore, payload: {} });
+            dispatch(RatesStore([]));
         }
     });
 }
--- frontend/src/js/reducers/RateReducer.ts
+++ frontend/src/js/reducers/RateReducer.ts
@@ -1,7 +1,8 @@
-import { RateActionsType } from '../constants/Rate';
 import { Rate } from '../api/model';
-import { produce } from 'immer';
-import { RateAction } from '../actions/RateActions';
+import {createAction, createReducer} from "@reduxjs/toolkit";
+
+export const RatesLoad = createAction('RatesLoad');
+export const RatesStore = createAction<Rate[]>('RatesStore');
 
 export interface RateState {
     readonly rateList: Rate[];
@@ -13,18 +14,15 @@ const initialState: RateState = {
     available: false,
 };
 
-export default function currencyReducer(state: RateState = initialState, action: RateAction) {
-    switch (action.type) {
-        case RateActionsType.RatesLoad:
-            return produce(state, draft => {
-                draft.available = false;
-            });
-        case RateActionsType.RatesStore:
-            return produce(state, draft => {
-                draft.available = true;
-                draft.rateList = action.payload;
-            });
-        default:
-            return state;
-    }
-}
+export default createReducer(initialState, (builder) => {
+    builder
+        .addCase(RatesLoad, (state) => {
+            state.available = false;
+        })
+        .addCase(RatesStore, (state, action) => {
+            state.available = true;
+            state.rateList = action.payload;
+        })
+});
+
+
