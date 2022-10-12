bc2d4b631d0e017773eaeb6a9e062e1f057cb209
Denis Chaplygin <akashihi@gmail.com>
2022-10-12 15:52:55 +0300

    Report reducer refactored with RTK

--- frontend/src/js/actions/ReportActions.ts
+++ frontend/src/js/actions/ReportActions.ts
@@ -1,21 +1,16 @@
-import { Action } from 'redux';
-
-import { ReportActionType } from '../constants/Report';
-import { TotalsReport } from '../api/model';
 import { wrap } from './base';
 import * as API from '../api/api';
+import {TotalsReportLoad, TotalsReportStore} from "../reducers/ReportReducer";
+import {NotifyError} from "../reducers/ErrorReducer";
 
-export interface ReportAction extends Action {
-    payload: {
-        totals: TotalsReport[];
-    };
-}
 export function loadTotalsReport() {
     return wrap(async dispatch => {
-        dispatch({ type: ReportActionType.TotalsReportLoad, payload: true });
+        dispatch(TotalsReportLoad());
         const result = await API.loadTotalsReport();
         if (result.ok) {
-            dispatch({ type: ReportActionType.TotalsReportStore, payload: { totals: result.val } });
+            dispatch(TotalsReportStore(result.val));
+        } else {
+            dispatch(NotifyError(result.val));
         }
     });
 }
