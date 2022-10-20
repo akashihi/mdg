6cf4801a7e37df1eedc5b295eb22071580d849f5
Denis Chaplygin <akashihi@gmail.com>
2022-10-20 23:43:20 +0300

    Implemented handling of unknown exceptions

--- backend/src/main/java/org/akashihi/mdg/api/v1/RestExceptionHandler.java
+++ backend/src/main/java/org/akashihi/mdg/api/v1/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.akashihi.mdg.api.v1;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.akashihi.mdg.api.v1.dto.Problem;
 import org.akashihi.mdg.dao.ErrorRepository;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +15,7 @@ import org.akashihi.mdg.entity.Error;
 
 @ControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private final ErrorRepository errorRepository;
 
@@ -21,9 +23,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return new Error(ex.getCode(), 500, "Undocumented error", "An error was emitted, which is not yet documented");
     }
 
-    @ExceptionHandler({MdgException.class})
-    public ResponseEntity<Object> handleRestException(MdgException ex, WebRequest request) {
-        var error = errorRepository.findById(ex.getCode()).orElseGet(() -> constructMissingProblem(ex));
+    public ResponseEntity<Problem> processError(Error error, WebRequest request) {
         String url = null;
         if (request instanceof ServletWebRequest swr) {
             url = swr.getRequest().getRequestURI();
@@ -32,5 +32,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         var headers = new HttpHeaders();
         headers.set("Content-Type", "application/vnd.mdg+json;version=1");
         return new ResponseEntity<>(problem, headers, error.getStatus());
+    }
+    @ExceptionHandler({MdgException.class})
+    public ResponseEntity<Problem> handleRestException(MdgException ex, WebRequest request) {
+        var error = errorRepository.findById(ex.getCode()).orElseGet(() -> constructMissingProblem(ex));
+        return processError(error, request);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Problem> handleGenericException(Exception ex, WebRequest request) {
+        log.warn(ex.getMessage(), ex);
+        var error = new Error("UNHANDLED_EXCEPTION", 500, ex.getMessage(), "An unhandled exception happened");
+        return processError(error, request);
     }
 }
