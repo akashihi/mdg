fd1898a54946301d295c7ba464ba86c10f960bc5
Denis Chaplygin <akashihi@gmail.com>
2022-10-26 20:03:06 +0300

    Linting errors fixed

--- backend/src/main/java/org/akashihi/mdg/api/util/CursorHelper.java
+++ backend/src/main/java/org/akashihi/mdg/api/util/CursorHelper.java
@@ -3,7 +3,6 @@ package org.akashihi.mdg.api.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.akashihi.mdg.api.v1.dto.TransactionCursor;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
