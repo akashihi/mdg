2390e608372e51c4353babb91c7f16bafb56e057
Denis Chaplygin <akashihi@gmail.com>
2023-01-06 13:49:33 +0200

    Budget tests converted to kotlin

--- backend/src/test/kotlin/org/akashihi/mdg/entity/BudgetEntryModeTest.kt
+++ backend/src/test/kotlin/org/akashihi/mdg/entity/BudgetEntryModeTest.kt
@@ -1,52 +1,44 @@
-package org.akashihi.mdg.entity;
+package org.akashihi.mdg.entity
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.util.*
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BudgetEntryModeTest {
-
+internal class BudgetEntryModeTest {
     @Test
-    void toDbValue() {
-        assertEquals("single", BudgetEntryMode.SINGLE.toDbValue());
-        assertEquals("even", BudgetEntryMode.EVEN.toDbValue());
-        assertEquals("prorated", BudgetEntryMode.PRORATED.toDbValue());
+    fun toDbValue() {
+        Assertions.assertEquals("single", BudgetEntryMode.SINGLE.toDbValue())
+        Assertions.assertEquals("even", BudgetEntryMode.EVEN.toDbValue())
+        Assertions.assertEquals("prorated", BudgetEntryMode.PRORATED.toDbValue())
     }
 
     @Test
-    void from() {
-        assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from("single"));
-        assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.from("even"));
-        assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.from("prorated"));
+    fun from() {
+        Assertions.assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from("single"))
+        Assertions.assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.from("even"))
+        Assertions.assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.from("prorated"))
     }
 
     @Test
-    void testFromBoolean() {
-        assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from(false, false));
-        assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from(false, true));
-        assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.from(true, false));
-        assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.from(true, true));
+    fun testFromBoolean() {
+        Assertions.assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from(false, false))
+        Assertions.assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.from(false, true))
+        Assertions.assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.from(true, false))
+        Assertions.assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.from(true, true))
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "false,false,single",
-            "false,true,single",
-            "true,false,even",
-            "true,true,prorated"})
-    void testFromEntry(Boolean even, Boolean prorated, String mode) {
-        assertEquals(BudgetEntryMode.from(mode), BudgetEntryMode.from(even, prorated));
+    @CsvSource("false,false,single", "false,true,single", "true,false,even", "true,true,prorated")
+    fun testFromEntry(even: Boolean?, prorated: Boolean?, mode: String?) {
+        Assertions.assertEquals(BudgetEntryMode.from(mode), BudgetEntryMode.from(even, prorated))
     }
 
     @Test
-    void flatten() {
-        assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.PRORATED, BudgetEntryMode.PRORATED)));
-        assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.EVEN, BudgetEntryMode.PRORATED)));
-        assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.EVEN, BudgetEntryMode.SINGLE)));
+    fun flatten() {
+        Assertions.assertEquals(BudgetEntryMode.PRORATED, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.PRORATED, BudgetEntryMode.PRORATED)))
+        Assertions.assertEquals(BudgetEntryMode.EVEN, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.EVEN, BudgetEntryMode.PRORATED)))
+        Assertions.assertEquals(BudgetEntryMode.SINGLE, BudgetEntryMode.flatten(Arrays.asList(BudgetEntryMode.PRORATED, BudgetEntryMode.EVEN, BudgetEntryMode.SINGLE)))
     }
 }
--- backend/src/test/kotlin/org/akashihi/mdg/service/BudgetServiceTest.kt
+++ backend/src/test/kotlin/org/akashihi/mdg/service/BudgetServiceTest.kt
@@ -1,40 +1,26 @@
-package org.akashihi.mdg.service;
+package org.akashihi.mdg.service
 
-import org.akashihi.mdg.entity.Budget;
-import org.akashihi.mdg.entity.BudgetEntry;
-import org.akashihi.mdg.entity.BudgetEntryMode;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.akashihi.mdg.entity.BudgetEntryMode
+import org.akashihi.mdg.service.BudgetService.Companion.getAllowedSpendings
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.math.BigDecimal
+import java.time.LocalDate
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class BudgetServiceTest {
-
+internal class BudgetServiceTest {
     @ParameterizedTest
-    @CsvSource({"25,100,25", "0,0,100", "0,100,0", "100,0,100", "100,100,100", "150,100,100"})
-    void testGetSpendingPercent(Long actualAmount, Long expectedAmount, Long expectedPercent) {
-        var actualPercent = BudgetService.getSpendingPercent(BigDecimal.valueOf(actualAmount), BigDecimal.valueOf(expectedAmount));
-
-        assertEquals(BigDecimal.valueOf(expectedPercent), actualPercent);
+    @CsvSource("25,100,25", "0,0,100", "0,100,0", "100,0,100", "100,100,100", "150,100,100")
+    fun testGetSpendingPercent(actualAmount: Long, expectedAmount: Long, expectedPercent: Long) {
+        val actualPercent = BudgetService.getSpendingPercent(BigDecimal.valueOf(actualAmount), BigDecimal.valueOf(expectedAmount))
+        Assertions.assertEquals(BigDecimal.valueOf(expectedPercent), actualPercent)
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "5,false,false,25,100,75",
-            "4,false,false,25,100,0",
-            "5,true,false,25,100,3",
-            "5,true,true,25,100,3",
-            "5,true,true,3,100,12",
-            "5,true,true,130,100,0",
-            "5,true,false,130,100,0",
-            "5,false,false,130,100,0"})
-    void analyzeRecommendedSpendings(Integer month, Boolean even, Boolean proration, Long actualAmount, Long expectedAmount, Long expectedSpendings) {
-        var actualAllowed = BudgetService.getAllowedSpendings(BigDecimal.valueOf(actualAmount), BigDecimal.valueOf(expectedAmount), LocalDate.of(2022, 5, 1), LocalDate.of(2022, 5, 31), LocalDate.of(2022, month,5 ), BudgetEntryMode.from(even, proration));
-
-        assertEquals(BigDecimal.valueOf(expectedSpendings), actualAllowed);
-
+    @CsvSource("5,false,false,25,100,75", "4,false,false,25,100,0", "5,true,false,25,100,3", "5,true,true,25,100,3", "5,true,true,3,100,12", "5,true,true,130,100,0", "5,true,false,130,100,0", "5,false,false,130,100,0")
+    fun analyzeRecommendedSpendings(month: Int, even: Boolean, proration: Boolean, actualAmount: Long, expectedAmount: Long, expectedSpendings: Long) {
+        val actualAllowed =
+            getAllowedSpendings(BigDecimal.valueOf(actualAmount), BigDecimal.valueOf(expectedAmount), LocalDate.of(2022, 5, 1), LocalDate.of(2022, 5, 31), LocalDate.of(2022, month, 5), BudgetEntryMode.from(even, proration))
+        Assertions.assertEquals(BigDecimal.valueOf(expectedSpendings), actualAllowed)
     }
 }
