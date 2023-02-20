378f099ce646cb0352b6dfdffe79aa2bbb1bd369
Denis Chaplygin <akashihi@gmail.com>
2023-02-20 22:06:44 +0200

    Added daily totals MQT for reporting

---
+++ backend/src/main/resources/conf/liquibase/31-historicaltotals.sql
@@ -0,0 +1,22 @@
+--liquibase formatted sql
+
+--changeset akashihi:1 splitStatements:false
+
+create materialized view historical_balance as
+select date(gs.dt)                                               as dt,
+       a.id                                                      as id,
+       sum(o.amount)                                             as amount,
+       sum(to_current_default_currency(a.currency_id, o.amount)) as primaryAmount
+from account as a
+         left outer join operation as o on (o.account_id = a.id)
+         inner join tx on (o.tx_id = tx.id)
+         cross join generate_series((select min(ts) from tx), now(), '1 day') gs(dt)
+where date(tx.ts) <= date(gs.dt)
+group by date(gs.dt), a.id
+with no data;
+
+create unique index historical_balance_dt on historical_balance(dt, id);
+
+
+
+
