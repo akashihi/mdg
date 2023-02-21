7f7ccd64ab48e3dbda9d6600e93bdf8335c1a571
Denis Chaplygin <akashihi@gmail.com>
2023-02-21 22:22:51 +0200

    Recursive MQT is faster to build

--- backend/src/main/resources/conf/liquibase/31-historicaltotals.sql
+++ backend/src/main/resources/conf/liquibase/31-historicaltotals.sql
@@ -2,23 +2,21 @@
 
 --changeset akashihi:1 splitStatements:false
 
-create materialized view historical_balance as
-select date(gs.dt)                                               as dt,
-       a.id                                                      as id,
-       sum(o.amount)                                             as amount,
-       sum(to_current_default_currency(a.currency_id, o.amount)) as primaryAmount
-from account as a
-         left outer join operation as o on (o.account_id = a.id)
-         inner join tx on (o.tx_id = tx.id)
-         cross join generate_series((select min(ts) from tx), now(), '1 day') gs(dt)
-where date(tx.ts) <= date(gs.dt)
-group by date(gs.dt), a.id
-with no data;
+create materialized view historical_balance as (with recursive totals as (select initial.dt, a.id as id, sum(o.amount) as amount
+                                                                   from account as a
+                                                                            cross join (select date(min(ts)) as dt from tx) as initial
+                                                                            right outer join operation o on a.id = o.account_id
+                                                                            inner join tx t on t.id = o.tx_id
+                                                                   group by initial.dt, a.id
+                                                                   union
+                                                                   select date(dt + interval '1 day'), t.id, t.amount + coalesce((select sum(o.amount) from operation as o, tx as tx where tx.id=o.tx_id and o.account_id=t.id and date(tx.ts) = dt + interval '1 day'), 0)
+                                                                   from totals as t
+                                                                   where dt + interval '1 day' <= date(now()))
+
+                                        select t.dt as dt, t.id as id, t.amount as amount, to_current_default_currency((select currency_id from account as a where a.id = t.id), t.amount) as primaryAmount
+                                        from totals as t) with data;
 
 create unique index historical_balance_dt on historical_balance(dt, id);
 
---changeset akashihi:2 splitStatements:false
-
-REFRESH MATERIALIZED VIEW historical_balance;
 
 
